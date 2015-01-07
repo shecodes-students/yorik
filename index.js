5e9c91ac878bcd73507479a9ae04a0f3d2c85249
Jan Bölsche <regular.gonzales@googlemail.com>
2015-01-07 21:31:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.body.innerHTML = '<canvas id="tty" class="screen" width="640" height="384"></canvas>';
+
+var Jor1k = require("jor1k/js/master/system");
+
+jor1kgui = new Jor1k({termid: "tty"});
+
+
