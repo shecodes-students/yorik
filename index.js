e8d042c44911b21f9c62ca434b741eb38a13c82c
Jan Bölsche <regular.gonzales@googlemail.com>
2015-01-10 19:51:58 +0100

    adjusted for changes in terminal API

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-document.body.innerHTML = '<canvas id="tty" class="screen" width="640" height="384"></canvas>';
+document.body.innerHTML = '<pre><canvas id="tty" class="screen" width="640" height="384"></canvas></pre>';
 
 var Jor1k = require("jor1k/js/master/system");
+var MackeTerm = require("jor1k/js/plugins/terminal-macke");
 
-jor1kgui = new Jor1k({termid: "tty"});
+jor1kgui = new Jor1k({term: new MackeTerm("tty")});
 
 
